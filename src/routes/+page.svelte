f9b78e59c4ec84432474478c4a37bd006b6cac55
Sankalp B <xsbsankalp@gmail.com>
2025-06-09 21:40:00 +0545

    blank adminator template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,6 @@
     export let data;
     </script>
 <h1>Welcome to SvelteKit {data.key}</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Visit <a href="/link2/">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Visit <a href="/admin">Admin Panel</a> to reach the Admin panel</p>
 
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Footer from "$lib/admin/footer.svelte";
+	import Sidebar from "$lib/admin/sidebar.svelte";
+	import Topbar from "$lib/admin/topbar.svelte";
+    
+</script>
+    <!-- @App Content -->
+    <!-- =================================================== -->
+    <div>
+      <Sidebar/>
+      <!-- #Main ============================ -->
+      <div class="page-container">
+       
+        <Topbar/>
+        <!-- ### $App Screen Content ### -->
+        <main class='main-content bgc-grey-100'>
+          <div id='mainContent'>
+            <div class="full-container">
+
+
+            </div>
+          </div>
+        </main>
+        <Footer/>
+      </div>
+    </div>
